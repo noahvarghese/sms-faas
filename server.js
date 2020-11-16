535ce57b0df3a1210a5a32c177649b4ec8dea6de
noahvarghese <varghese.noah@gmail.com>
2020-11-15 21:13:03 -0500

    added statement of authorships

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// StAuth10065: I Noah Varghese, 000753196 certify that this material is my original work.
+// No other person's work has been used without due acknowledgement.
+// I have not made my work available to anyone else.
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
